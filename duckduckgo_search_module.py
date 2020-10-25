39bbeff8266504c11259437676972204fb6b3c06
suzanagi <realyutanemoto@gmail.com>
2020-10-26 01:28:55 +0900

    created the function to retrieve the search result page from DuckDuckGo

--- duckduckgo_search_module.py
+++ duckduckgo_search_module.py
@@ -1,11 +1,48 @@
-import requests
 import sys
+import json
+from urllib import request
+from urllib import parse
 from bs4 import BeautifulSoup
 from result_item import ResultItem
 
+def retrieve_result_page(query: str) -> str:
+    '''
+    Parameters
+    ----------
+    query : str
+        search query string to be sent to DuckDuckGo
+        can contain space character and no need to concatenate with '+'
+        ex. "hello world"
+
+    Returns
+    ----------
+    result_page : str
+        obtained HTML result page in string
+        ex. "<!DOCTYPE html PUBLIC "-//W3C...</body></html>"
+    '''
+    # Put the query into the form data (post parameter) and it should be encoded binary
+    # Look at "Data" section on the page https://docs.python.org/3.7/howto/urllib2.html
+    data = parse.urlencode({
+        "q": query,
+    }).encode('ascii')
+
+    # To access their result without using web browser just use html version of their page
+    url = "https://html.duckduckgo.com/html/"
+
+    # Method should be POST to retrieve their result page correctly
+    request_to_throw = request.Request(url, data, method="POST")
+
+    # In case it fails to retrieve the result, return an empty string
+    result_page = ""
+
+    with request.urlopen(request_to_throw) as response:
+        result_page = response.read().decode('utf-8')
+
+    return result_page
+
 def search(query):
-    # Get the Yahoo search result page for the query
-    page = requests.get('https://search.yahoo.com/search?p='+query)
+    # Get the DuckDuckGo search result page for the query
+    page = retrieve_result_page(query)
     # Prepare a list for returning the search results
     result = list()
     # Check the result page encoding to use it in BeautifulSoup composition
